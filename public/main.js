1abdd817ccde103e6a68e6d1e00e06d617f82916
Tomi Helin <tomi.helin@student.lut.fi>
2024-11-14 21:02:09 +0200

    Task 4

---
+++ public/main.js
@@ -0,0 +1,48 @@
+
+function initialize() {
+
+    const addUserButton = document.getElementById("add-user")
+
+    addUserButton.addEventListener("click", async function() {
+        const username = document.getElementById("name")
+        const email = document.getElementById("email")
+
+        const data = await fetch("http://localhost:3000/users", {
+            method: "post",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: '{"username": "'+username+'", "email": "'+email+'"}'
+        })
+
+        const json = await data.json()
+        console.log(json)
+    })
+
+    const getUsersButton = document.getElementById("getUsers")
+
+    getUsersButton.addEventListener("click", async function() {
+        try {
+            const response = await fetch("http://localhost:3000/users")
+            const users = await response.json()
+
+            console.log(users)
+
+            const list = document.getElementById("userList")
+
+            list.innerHTML = ""
+
+            users.forEach(user => {
+                const li = document.createElement("li");
+                li.textContent = `${user.name} - ${user.email}`
+                list.appendChild(li)
+            })
+        } catch (error) {
+            console.error("error:", error)
+
+        }
+    })
+
+}
+
+initialize()
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import {Request, Response, Router} from "express"
 
 const router: Router = Router()
 
+type TUser = {
+    name: string;
+    email: string;
+}
+
+const users: TUser[] = [];
+
 router.get("/hello", (req: Request, res: Response) => {
     res.json({
         msg: "Hello World!"
@@ -22,6 +29,28 @@ router.post("/sum", (req: Request, res: Response) => {
     res.json({
         sum
     })
+})
+
+router.post("/users", (req: Request, res: Response) => {
+    const { name, email} = req.body
+
+    const addUser: TUser = { name, email }
+    users.push(addUser);
+
+    res.json({
+        message: "User successfully added"
+    })
+
+    console.log("Add " + users)
+})
+
+router.get("/users", (req: Request, res: Response) => {
+
+    console.log("Get " + users)
+
+    const data = JSON.stringify(users);
+
+    res.status(201).json(data)
 })
 
 export default router
